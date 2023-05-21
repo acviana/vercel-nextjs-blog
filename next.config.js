072be968f9e7131e1bd4da43c65651e945a2effa
Alex C. Viana <alexcostaviana@gmail.com>
2023-05-20 19:37:43 -0500

    Moved to jsx theme config file and added latex support. (#5)

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.jsx',
+  latex: true
+})
+ 
 module.exports = withNextra()
+ 
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
