794d4e40ed570e4e198cea64db9f7a70b6d960bb
Alex C. Viana <alexcostaviana@gmail.com>
2024-10-04 09:38:19 -0500

    2024 10 03 version bump (#32)
    * Version bump and subsequent updates. Compiles locally but server crashes when loading.
    
    * Renamed _app.mdx to _app.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-const withNextra = require('nextra')({
+import nextra from 'nextra';
+
+const withNextra = nextra({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.jsx',
+  defaultShowCopyCode: true,
+  readingTime: true,
   latex: true
 })
  
-module.exports = withNextra()
+export default withNextra({
+  reactStrictMode: true,
+  cleanDistDir: true
+})
  
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
